ea2c9b1e8abffca9c7f89e61b93a38ae894bf15c
Alexandra Romensky <alexandra.romensky@gmail.com>
2023-12-12 14:49:55 +0100

    medeling and fast with the new model (3 products forecasted

---
+++ api/fast_copy_tft.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, UploadFile, File
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from bakery_sales.modeling import prediction
+
+from darts.models.forecasting.tft_model import TFTModel
+
+app = FastAPI()
+
+# specify path to pytorch model .pt file
+pt_file_path = 'weights/tft_model_final.pt'
+# new tft model
+
+# load pytorch model and set to eval mode
+model = TFTModel.load(pt_file_path)
+# model.eval()
+
+app.state.model = model
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@app.post("/upload")
+def upload_files(files: List[UploadFile] = File(...)):
+    dataframes = []
+    for file in files:
+        df = pd.read_csv(file.file)
+        file.file.close()
+        dataframes.append(df)
+    pred = prediction(dataframes[0], dataframes[1], app.state.model)
+    return {"output": pred}
+
+
+
+@app.get("/")
+def root():
+    return {'greeting': 'Hello'}
